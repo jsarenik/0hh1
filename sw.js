5c6aca75960f2ea53f647351f8f0cc54366c74b8
Ján Sáreník <jajomojo@gmail.com>
2026-07-18 12:44:03 +0200

    Implement service worker activate event
    Added activate event to clean up old caches and take control of open tabs.
    
    https://share.google/aimode/LJff7DNlRsFrtlBub

--- sw.js
+++ sw.js
@@ -60,7 +60,22 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting()) // Forces activation
+  );
+});
+
+// Activate Event (Cleans up old caches)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Takes control of open tabs immediately
   );
 });
 
